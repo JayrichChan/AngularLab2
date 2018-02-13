5332b1503a5e559f215bc30d054912af0f864f37
CHAN JAYRICH - STUDENT <G00341968@gmit.ie>
2018-02-13 18:06:17 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  i:number = 0;
   title = 'app';
+  hidden = false;
+Count(){
+  this.i++;
+}
+
+Show(){
+  if(this.hidden == true)
+  {
+  this.hidden = false;
+  }
+  else{
+  this.hidden = true;
+  }
+}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListcomponentComponent } from './listcomponent/listcomponent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListcomponentComponent
   ],
   imports: [
     BrowserModule
